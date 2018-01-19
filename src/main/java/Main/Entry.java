3161cdf52fe189d092cf2df66996b8d113e06be9
MiaoMiao <360382992@qq.com>
2018-01-19 20:36:16 +0800

    first commit Signed-off-by: MiaoMiao <360382992@qq.com>

---
+++ src/main/java/Main/Entry.java
@@ -0,0 +1,16 @@
+package Main;
+
+import java.io.IOException;
+
+public class Entry{
+
+	
+	public static void main(String[] args) throws IOException {
+            System.out.println(args);
+
+	}
+
+
+	
+	
+}
