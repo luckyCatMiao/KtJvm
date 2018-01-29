c86cb8cc1a66b8a14b558c288f39b0fecb25ab83
MiaoMiao <360382992@qq.com>
2018-01-29 23:48:17 +0800

    refine Signed-off-by: MiaoMiao <360382992@qq.com>

--- src/main/java/KtEx/ObjectEx.kt
+++ src/main/java/KtEx/ObjectEx.kt
@@ -1,7 +1,12 @@
 package KtEx
 
+import kotlin.reflect.KProperty
+
 inline fun Int.times(action:()->Unit){
     for(i in 0..this-1){
         action()
     }
 }
+
+fun <T>KProperty<T>.valueOfObject(o:Any)=this.call(o)
+
--- src/main/java/Parser/ConstantPool/ConstantPoolParser.kt
+++ src/main/java/Parser/ConstantPool/ConstantPoolParser.kt
@@ -1,10 +1,9 @@
 package Parser.ConstantPool
 
-import KtEx.times
+import KtEx.valueOfObject
 import Parser.DataReader
 import Parser.JavaClass
-import com.google.common.base.Preconditions
-import com.google.common.base.Preconditions.*
+import com.google.common.base.Preconditions.checkState
 import io.reactivex.Observable
 import kotlin.reflect.KFunction
 import kotlin.reflect.KProperty
@@ -22,36 +21,61 @@ class ConstantPoolParser(val javaclass:JavaClass,val reader:DataReader) {
     private fun parseConstant() {
         val tag=reader.readUnsignedByte()
         //use reflection to call function
-
         val klass=ConstantTag::class.java.kotlin
         Observable.fromIterable(klass.members)
-                .filter { it is  KProperty}
-                .filter { it.call(ConstantTag)==tag }
-                .map { "parse"+it.name.replace("CONSTANT_","") }
+                .filter { Helper.isProperty(it)}
+                .cast(KProperty::class.java)
+                .filter { Helper.equal(it.valueOfObject(ConstantTag),tag) }
+                .map { Helper.toMethodName(it) }
                 .subscribe{
                     //call functions by names
-                    callByName(it)
+                    Helper.callMethodByName(it,this)
                 }
 
 
     }
 
-    private fun callByName(name: String) {
-        val klass=this::class.java.kotlin
-        Observable.fromIterable(klass.members)
-                .filter { it is KFunction}
-                .filter { it.name.equals(name) }
-                .toList()
-                .subscribe({
-                    println(it.size)
-                    checkState(it.size==1,"internal error!method ${name} do not exist!")
-                    it[0].call(this)
-                },{
-                    RuntimeException("rxjava error")
-                })
-    }
+
 
      fun parseMethodref(){
         println("6666")
     }
+
+    /**
+     * internal helper object
+     */
+    object Helper
+    {
+        fun isProperty(it:Any)=it is KProperty<*>
+        fun isFunction(it:Any)=it is KFunction<*>
+        fun equal(a:Any?,b:Any?):Boolean{
+             if(a==null||b==null)
+            {
+                return false
+            }else{
+                return a.equals(b)
+            }
+
+        }
+
+        fun toMethodName(it: KProperty<*>)="parse"+it.name.replace("CONSTANT_","")
+
+
+        fun callMethodByName(name: String,target:Any) {
+            val klass=target::class.java.kotlin
+            Observable.fromIterable(klass.members)
+                    .filter { isFunction(it)}
+                    .cast(KFunction::class.java)
+                    .filter { equal(it.name,name) }
+                    .toList()
+                    .subscribe({
+                        println(it.size)
+                        checkState(it.size==1,"internal error!method ${name} do not exist!")
+                        it[0].call(target)
+                    },{
+                        RuntimeException("rxjava error")
+                    })
+        }
+    }
+
 }
